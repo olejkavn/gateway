d6001f4d01cadee36857f52e59ec9432691b1ba9
otymkovskyi <otymkovskyi@playtika.com>
2022-08-10 16:28:38 +0200

    initial commit

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 mvn clean install
-docker build -t pet-project/gateway .
-docker tag pet-project/gateway registry.digitalocean.com/pet-project/gateway
-docker push registry.digitalocean.com/pet-project/gateway
+docker build -t gateway .
+docker tag gateway otymkovskyi/gateway
+docker push otymkovskyi/gateway
 kubectl delete deploy gateway
 kubectl create -f kube-manifests/deployment.yaml
 kubectl get pods
